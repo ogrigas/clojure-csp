cb3b73c2922ad440841549f5b8b52f03fbeec9bf
Osvaldas Grigas <osvaldas@inventi.lt>
2016-02-02 20:42:46 +0200

    fix actor example

--- src/csp/oop.clj
+++ src/csp/oop.clj
@@ -5,27 +5,31 @@
 (defn new-instance [actor-fn initial-state]
   (let [state (volatile! initial-state)
         mailbox (chan 100)]
-    (go-loop [] (vswap! state actor-fn (<! mailbox)) (recur))
+    (go-loop [] (actor-fn state (<! mailbox)) (recur))
     (fn [& args] (>!! mailbox (vec args)))))
 
 (defn account [state message]
   (match message
     ['deposit amount]
-    (update state :balance + amount)
-    
+    (vswap! state update :balance + amount)
+
     ['withdraw amount]
-    (if (<= amount (:balance state))
-      (update state :balance - amount)
-      state)
-    
+    (if (<= amount (:balance @state))
+      (vswap! state update :balance - amount)
+      (println "Overdrafts not allowed"))
+
     ['print-report]
-    (println "Account" (:number state)
-             "balance:" (:balance state))))
+    (println "Account" (:number @state)
+             "balance:" (:balance @state))))
 
 
 (def acc (new-instance account {:number "LT1234" :balance 0}))
+
 (acc 'deposit 100)
+(acc 'print-report)
+
 (acc 'withdraw 20)
-(acc 'withdraw 5)
+(acc 'print-report)
+
 (acc 'withdraw 99999)
 (acc 'print-report)
