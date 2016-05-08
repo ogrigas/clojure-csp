519c50ad9e429d70ea47b0828894359b0ec30d7b
ogrigas <o.grigas@gmail.com>
2016-05-08 20:02:33 +0300

    poetry (draft)

---
+++ src/poetry.clj
@@ -0,0 +1,18 @@
+(ns poetry)
+
+(let [the count
+      of (the [count repeat count repeat count repeat count repeat count repeat])
+      hate (the [count repeat count repeat count repeat count repeat])
+      love (partial * of)
+      grow +
+      coal of
+      drawings of
+      say char(coal drawings)
+      S (say (grow (love hate) (the [count of repeat])))
+      H (say (grow (love (dec hate)) (the [repeat count])))
+      A \A
+      K \K
+      E \E
+      P \P
+      R \R]
+  (print [S H A K E S P E A R E]))
